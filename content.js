bbe8c551f1a26db0cf67dc5e52dab72cda8c6486
Alex-zhg <86493680+Alex-zhg@users.noreply.github.com>
2021-06-25 15:32:07 -0400

    Update content.js

--- content.js
+++ content.js
@@ -6,8 +6,8 @@ chrome.runtime.onMessage.addListener(
     //            "from a content script:" + sender.tab.url :
     //            "from the extension");
     //console.log(request.greeting);
-    var messageText = request.greeting
-    if (request.greeting == "https://app.peardeck.com/join") {
+    var messageText = request.greeting;
+    if (request.greeting === "https://app.peardeck.com/join") {
 
       let filenames = [
         "angryPearSmolFinal.png",
@@ -30,13 +30,13 @@ chrome.runtime.onMessage.addListener(
       //console.log(request.greeting);
       sendResponse({farewell: "goodbye"});
       //console.log("end of content.js");
-    } else if (request.greeting == 'HAPPYhttps://app.peardeck.com/join') {
+    } else if (request.greeting === "HAPPYhttps://app.peardeck.com/join") {
         var path = chrome.extension.getURL("pears/happyPear.png");
         document.getElementsByClassName("join__logo")[0].src = path;
         sendResponse({farewell: "goodbye"});
         //console.log('button -> happy join page');
         //console.log(path);
-    } else if (request.greeting == "MADhttps://app.peardeck.com/join") {
+    } else if (request.greeting === "MADhttps://app.peardeck.com/join") {
         var path = chrome.extension.getURL("pears/angryPearSmolFinal.png");
         document.getElementsByClassName("join__logo")[0].src = path;
         sendResponse({farewell: "goodbye"});
@@ -48,40 +48,40 @@ chrome.runtime.onMessage.addListener(
         sendResponse({farewell: "goodbye"});
         //console.log('button -> sad join page');
         //console.log(path);
-    } else if (request.greeting == 'KINDASADhttps://app.peardeck.com/join') {
+    } else if (request.greeting === "KINDASADhttps://app.peardeck.com/join") {
       var path = chrome.extension.getURL("pears/kindaSadPear.png");
       document.getElementsByClassName("join__logo")[0].src = path;
       sendResponse({farewell: "goodbye"});
       //console.log('button -> kinda sad join page');
       //console.log(path);
-    } else if (messageText.slice(0, 39) == 'HAPPYhttps://app.peardeck.com/presenter') {
+    } else if (messageText.slice(0, 39) === "HAPPYhttps://app.peardeck.com/presenter") {
         var path = chrome.extension.getURL("pears/join-modal-logo.png");
-        document.querySelector('.alpha-modal__peary img').src = path;
+        document.querySelector(".alpha-modal__peary img").src = path;
         sendResponse({farewell: "goodbye"});
         console.log(path);
-        console.log('button -> happy code page');
-    } else if (messageText.slice(0, 37) == 'MADhttps://app.peardeck.com/presenter') {
+        console.log("button -> happy code page");
+    } else if (messageText.slice(0, 37) === "MADhttps://app.peardeck.com/presenter") {
         var path = chrome.extension.getURL("pears/join-modal-pear-mad.png");
-        document.querySelector('.alpha-modal__peary img').src = path;
+        document.querySelector(".alpha-modal__peary img").src = path;
         sendResponse({farewell: "goodbye"});
         console.log(path);
         console.log(messageText.slice(0, 37));
-        console.log('button -> angry code page');
-    } else if (messageText.slice(0, 42) == 'KINDASADhttps://app.peardeck.com/presenter') {
+        console.log("button -> angry code page");
+    } else if (messageText.slice(0, 42) === "KINDASADhttps://app.peardeck.com/presenter") {
         var path = chrome.extension.getURL("pears/join-modal-logo-kinda-sad.png");
-        document.querySelector('.alpha-modal__peary img').src = path;
+        document.querySelector(".alpha-modal__peary img").src = path;
         sendResponse({farewell: "goodbye"});
         console.log(path);
         console.log(messageText.slice(0, 37));
-        console.log('button -> kinda sad code page');
-    } else if (messageText.slice(0, 37) == 'SADhttps://app.peardeck.com/presenter') {
+        console.log("button -> kinda sad code page");
+    } else if (messageText.slice(0, 37) === "SADhttps://app.peardeck.com/presenter") {
         var path = chrome.extension.getURL("pears/join-modal-logo-sad.png");
-        document.querySelector('.alpha-modal__peary img').src = path;
+        document.querySelector(".alpha-modal__peary img").src = path;
         sendResponse({farewell: "goodbye"});
         console.log(path);
         console.log(messageText.slice(0, 37));
-        console.log('button -> angry code page');
-    } else if (request.greeting == 'RANDOMhttps://app.peardeck.com/join') {
+        console.log("button -> angry code page");
+    } else if (request.greeting == "RANDOMhttps://app.peardeck.com/join") {
       let filenames = [
         "angryPearSmolFinal.png",
         "sadPearSmol.png",
@@ -89,7 +89,7 @@ chrome.runtime.onMessage.addListener(
       ];
 
       let r = Math.floor(Math.random() * filenames.length);
-      let file = 'pears/' + filenames[r];
+      let file = "pears/" + filenames[r];
       var path = chrome.extension.getURL(file);
       console.log(path);
       console.log(r);
@@ -100,7 +100,7 @@ chrome.runtime.onMessage.addListener(
       console.log(request.greeting);
       sendResponse({farewell: "goodbye"});
       console.log("end of content.js");
-    } else if (messageText.slice(0, 42) == 'RANDOMhttps://app.peardeck.com/presenter') {
+    } else if (messageText.slice(0, 42) === "RANDOMhttps://app.peardeck.com/presenter") {
 
       let filenames = [
         "join-modal-pear-mad.png",
@@ -114,12 +114,12 @@ chrome.runtime.onMessage.addListener(
       console.log(path);
       console.log(r);
 
-      function pollDOM () {
-        const el = document.querySelector('.alpha-modal__peary img');
+   function pollDOM () {
+        const el = document.querySelector(".alpha-modal__peary img");
 
         if (el != null) {
           // Do something
-          document.querySelector('.alpha-modal__peary img').src = path;
+          document.querySelector(".alpha-modal__peary img").src = path;
         } else {
           setTimeout(pollDOM, 200); // try again in milliseconds
         }
